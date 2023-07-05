15b20759c8c8f7cc4efa12d00ac3e90aeef252d0
ise-208 <seed.0ri00@gmail.com>
2023-07-05 20:56:59 +0900

    log4j loggerの追加

--- src/main/java/com/example/springaopsample/AopController.java
+++ src/main/java/com/example/springaopsample/AopController.java
@@ -1,5 +1,7 @@
 package com.example.springaopsample;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AopController {
 
     AopComponent aopComponent;
+    Logger logger = LogManager.getLogger("CONSOLE_JSON_APPENDER");
 
     public AopController(AopComponent aopComponent) {
         this.aopComponent = aopComponent;
@@ -15,6 +18,11 @@ public class AopController {
     @GetMapping("/hello")
     public void hello() {
         aopComponent.doService();
+    }
+
+    @GetMapping("/json")
+    public void json() {
+        logger.info("info message");
     }
 
 }
